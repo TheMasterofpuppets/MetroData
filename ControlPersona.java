b597575b0492acc80d694951870226e188daed2b
TheMasterofpuppets <mateo.amaya@protonmail.com>
2022-11-11 12:05:19 -0500

    modicado

--- ControlPersona.java
+++ ControlPersona.java
@@ -1,3 +1,21 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
 public class ControlPersona{
+
+    public static ArrayList<Persona> personas = new ArrayList<Persona>();
+
+    
+    /* 
+    todo: Crear metodo para leer lista de archivos 
+    todo: Crear metodo para Agregar persona
+    todo: metodo para modificar persona
+    */ 
+      
+
     
 }
